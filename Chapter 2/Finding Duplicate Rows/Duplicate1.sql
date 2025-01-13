cf77a0bcfae4105e7a7970de2a389fe995401b60
SFahimRahman <sfr.fahim@gmail.com>
2025-01-13 17:32:24 +0600

    Update Duplicate1.sql

--- Chapter 2/Finding Duplicate Rows/Duplicate1.sql
+++ Chapter 2/Finding Duplicate Rows/Duplicate1.sql
@@ -1,7 +1,7 @@
 SELECT Customer.CustomerID,
   Customer.FirstName,
   Customer.LastName,
-  COUNT(1) as NUM_ORDERS
+  COUNT(1) as NUM_ORDER
 FROM Customer
   JOIN Orders ON Customer.CustomerID = Orders.CustomerID
 GROUP BY Customer.CustomerID,
